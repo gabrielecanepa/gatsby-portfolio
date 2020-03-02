d798435bc841430105404e140e2654a66b01b4c1
Gabriele Canepa <git@gabrielecanepa.com>
2020-03-02 09:49:59 +0100

    Fix console warnings

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,21 @@
 /* eslint-disable no-undef */
 const path = require('path')
 
-// Enable absolute imports
-exports.onCreateWebpackConfig = ({ actions }) => {
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+  // Enable absolute imports
   actions.setWebpackConfig({
     resolve: {
       modules: [path.resolve(__dirname, 'src'), 'node_modules'],
     },
   })
+  // Fix hot-reload warning (see https://github.com/gatsbyjs/gatsby/issues/11934)
+  if (stage.startsWith('develop')) {
+    actions.setWebpackConfig({
+      resolve: {
+        alias: {
+          'react-dom': '@hot-loader/react-dom',
+        },
+      },
+    })
+  }
 }
