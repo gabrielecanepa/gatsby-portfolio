2aec579478873ed0cb168dbc4a23f2fb5fe6ea5c
Gabriele Canepa <git@gabrielecanepa.com>
2020-03-03 13:01:47 +0100

    Fix hot reload

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,20 +2,14 @@
 const path = require('path')
 
 exports.onCreateWebpackConfig = ({ stage, actions }) => {
-  // Enable absolute imports
   actions.setWebpackConfig({
     resolve: {
+      alias: {
+        // Fix hot-reload warning (see https://github.com/gatsbyjs/gatsby/issues/11934)
+        'react-dom': stage.startsWith('develop') ? '@hot-loader/react-dom' : 'react-dom',
+      },
+      // Enable absolute imports
       modules: [path.resolve(__dirname, 'src'), 'node_modules'],
     },
   })
-  // Fix hot-reload warning (see https://github.com/gatsbyjs/gatsby/issues/11934)
-  if (stage.startsWith('develop')) {
-    actions.setWebpackConfig({
-      resolve: {
-        alias: {
-          'react-dom': '@hot-loader/react-dom',
-        },
-      },
-    })
-  }
 }
