42baaa8374594b7ffe49ea97a0c31068f7ef68ce
Gabriele Canepa <git@gabrielecanepa.com>
2020-03-09 18:47:12 +0100

    Use persisted state to manage theme

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,7 +5,7 @@ exports.onCreateWebpackConfig = ({ stage, actions }) => {
   actions.setWebpackConfig({
     resolve: {
       alias: {
-        // Fix hot-reload warning (see https://github.com/gatsbyjs/gatsby/issues/11934)
+        // Fix hot-reload warnings (https://github.com/gatsbyjs/gatsby/issues/11934)
         'react-dom': stage.startsWith('develop') ? '@hot-loader/react-dom' : 'react-dom',
       },
       // Enable absolute imports
--- src/layout/index.js
+++ src/layout/index.js
@@ -4,13 +4,12 @@ import Inner from './inner'
 import React from 'react'
 import SEO from './seo'
 import Style from './style'
-import { StoreProvider } from 'ext/hooks/store'
 
 const Layout = ({ children }) => (
-  <StoreProvider>
+  <>
     <SEO />
     <Style>{children}</Style>
-  </StoreProvider>
+  </>
 )
 
 export default Layout
