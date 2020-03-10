a7de73fcd73a05e3a6d47ac4ff51362460155b1f
Gabriele Canepa <git@gabrielecanepa.com>
2020-03-10 19:18:19 +0100

    Remove loader icon

--- src/images/index.js
+++ src/images/index.js
@@ -1,7 +1,6 @@
 import Avatar from './avatar.svg'
 import LeWagonLogo from './lewagon-logo.svg'
-import Loader from './loader.svg'
 import icons from './icons'
 import leWagonBg from './lewagon-bg.png'
 
-export { Avatar, icons, leWagonBg, LeWagonLogo, Loader }
+export { Avatar, icons, leWagonBg, LeWagonLogo }
